1b3b37f50356f25abda0c9651c43ae7797122ac1
Devon Deason <devon.d.deason@gmail.com>
2019-11-20 17:53:20 -0700

    build out component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <div>Hi there!</div>;
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Devon
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice Blog Post</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
